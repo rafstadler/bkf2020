4cbe2901c55cb97997d74ee59956a496227e9e19
raphael stadler <raph.stadler@gmail.com>
2020-03-02 14:06:29 +0100

    debut members

--- src/Components/Members/Members.jsx
+++ src/Components/Members/Members.jsx
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import {Container,Row,Col,Image} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Members.css';
+import droite from './flechedroite.png';
+import gauche from './flechegauche.png';
+import dudu from './dudu.png';
+
 
 class Header extends Component {
 
     render() {
         return (
-            <div></div>
+            <Row>
+                <Col xs="1">
+                    <Image className="image" src={gauche} fluid></Image>
+                </Col>
+                <Col xs="4">
+                    <Image className="image" src={dudu} fluid></Image>
+                </Col>
+                <Col xs="6">
+                </Col>
+                <Col xs="1">
+                    <Image className="image" src={droite} fluid></Image>
+                </Col>
+            </Row>
         );
     }
 }
